c96db8f7807ab9ac639f977366867da7f4e45a0b
Starlone <starlone.passos@gmail.com>
2021-08-16 16:41:31 -0300

    Add matterjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bodies, Engine, Render, World, Runner } from 'matter-js';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -8,7 +9,7 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'star-gameengine-editor';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -17,5 +18,37 @@ export class AppComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
+
+  ngOnInit() {
+    this.demo();
+  }
+
+  demo() {
+    var runner = Runner.create();
+    var engine = Engine.create()
+    var render = Render.create({
+      element: document.getElementById('canvas') || undefined,
+      engine: engine,
+      options: {
+        width: 800,
+        height: 400,
+        wireframes: false,
+      },
+    })
+    var boxA = Bodies.rectangle(400, 200, 80, 80)
+    var ballA = Bodies.circle(380, 100, 40, {})
+    var ballB = Bodies.circle(460, 10, 40, {})
+    var ground = Bodies.rectangle(400, 380, 810, 60, {
+      isStatic: true,
+    })
+    World.add(engine.world, [boxA, ballA, ballB, ground])
+    Render.run(render)
+
+    Runner.run(runner, engine);
+    console.log('oi');
+
+  }
 }
