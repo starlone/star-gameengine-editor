72b448822b8ea2767b469e7e2dfc22f463f886ea
Starlone <starlone.passos@gmail.com>
2021-11-17 15:54:40 -0300

    feat: create objects and child objects

--- src/app/pages/edit/panels/panel-objects-tree/panel-objects-tree.component.ts
+++ src/app/pages/edit/panels/panel-objects-tree/panel-objects-tree.component.ts
@@ -4,17 +4,18 @@ import {
   Component,
   ElementRef,
   EventEmitter,
-  Injectable,
   Input,
   Output,
   ViewChild,
 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import {
   MatTreeFlatDataSource,
   MatTreeFlattener,
 } from '@angular/material/tree';
-import { BehaviorSubject } from 'rxjs';
-import { GameObject, IGameObjectOptions } from 'star-gameengine';
+import { GameObject } from 'star-gameengine';
+import { DialogNewrectComponent } from '../../dialogs/dialog-newrect/dialog-newrect.component';
+import { ChecklistDatabase } from './panel-objects-tree.database';
 
 /** Flat to-do item node with expandable and level information */
 export class GameObjectFlatNode {
@@ -22,151 +23,6 @@ export class GameObjectFlatNode {
   level: number = 0;
   expandable: boolean = false;
   uid: string = '';
-}
-
-/**
- * Checklist database, it can build a tree structured Json object.
- * Each node in Json object represents a to-do item or a category.
- * If a node is a category, it has children items and new items can be added under the category.
- */
-@Injectable()
-export class ChecklistDatabase {
-  dataChange = new BehaviorSubject<GameObject[]>([]);
-
-  get data(): GameObject[] {
-    return this.dataChange.value;
-  }
-
-  constructor() {
-    const data: GameObject[] = [];
-    this.dataChange.next(data);
-  }
-
-  load(objects: GameObject[]) {
-    this.dataChange.next(objects);
-  }
-
-  /** Add an item to to-do list */
-  insertItem(parent: GameObject, options: IGameObjectOptions): GameObject {
-    const newItem = new GameObject(options);
-    parent.children.push(newItem);
-    this.dataChange.next(this.data);
-    return newItem;
-  }
-
-  insertItemAbove(node: GameObject, options: IGameObjectOptions): GameObject {
-    const parentNode = this.getParentFromNodes(node);
-    const newItem = new GameObject(options);
-    if (parentNode != null) {
-      parentNode.children.splice(parentNode.children.indexOf(node), 0, newItem);
-    } else {
-      this.data.splice(this.data.indexOf(node), 0, newItem);
-    }
-    this.dataChange.next(this.data);
-    return newItem;
-  }
-
-  insertItemBelow(node: GameObject, options: IGameObjectOptions): GameObject {
-    const parentNode = this.getParentFromNodes(node);
-    const newItem = new GameObject(options);
-    if (parentNode != null) {
-      parentNode.children.splice(
-        parentNode.children.indexOf(node) + 1,
-        0,
-        newItem
-      );
-    } else {
-      this.data.splice(this.data.indexOf(node) + 1, 0, newItem);
-    }
-    this.dataChange.next(this.data);
-    return newItem;
-  }
-
-  getParentFromNodes(node: GameObject): GameObject | undefined {
-    for (const currentRoot of this.data) {
-      const parent = this.getParent(currentRoot, node);
-      if (parent != null) {
-        return parent;
-      }
-    }
-    return undefined;
-  }
-
-  getParent(currentRoot: GameObject, node: GameObject): GameObject | undefined {
-    if (currentRoot.children && currentRoot.children.length > 0) {
-      for (const child of currentRoot.children) {
-        if (child === node) {
-          return currentRoot;
-        } else if (child.children && child.children.length > 0) {
-          const parent = this.getParent(child, node);
-          if (parent != null) {
-            return parent;
-          }
-        }
-      }
-    }
-    return undefined;
-  }
-
-  updateItem(node: GameObject, name: string) {
-    node.name = name;
-    this.dataChange.next(this.data);
-  }
-
-  deleteItem(node: GameObject) {
-    this.deleteNode(this.data, node);
-    this.dataChange.next(this.data);
-  }
-
-  private removeOldParent(from: GameObject) {
-    const parent = this.getParentFromNodes(from);
-    const children = parent != null ? parent.children : this.data;
-    this.deleteNode(children, from);
-  }
-
-  moveItem(from: GameObject, to: GameObject): GameObject {
-    this.removeOldParent(from);
-
-    to.children.push(from);
-    from.setParent(to);
-    this.dataChange.next(this.data);
-    return from;
-  }
-
-  moveItemAtIndex(from: GameObject, to: GameObject, increment: number) {
-    this.removeOldParent(from);
-
-    const parentNode = this.getParentFromNodes(to);
-    const objs = parentNode != null ? parentNode.children : this.data;
-
-    const index = objs.indexOf(to);
-    objs.splice(index + increment, 0, from);
-
-    from.setParent(parentNode);
-    this.dataChange.next(this.data);
-    return from;
-  }
-
-  moveItemAbove(from: GameObject, to: GameObject): GameObject {
-    return this.moveItemAtIndex(from, to, 0);
-  }
-
-  moveItemBelow(from: GameObject, to: GameObject): GameObject {
-    return this.moveItemAtIndex(from, to, 1);
-  }
-
-  deleteNode(nodes: GameObject[], nodeToDelete: GameObject) {
-    const index = nodes.indexOf(nodeToDelete, 0);
-    if (index > -1) {
-      nodes.splice(index, 1);
-    } else {
-      nodes.forEach((node) => {
-        if (node.children && node.children.length > 0) {
-          this.deleteNode(node.children, nodeToDelete);
-        }
-      });
-    }
-  }
 }
 
 @Component({
@@ -207,7 +63,7 @@ export class PanelObjectsTreeComponent implements AfterViewInit {
   dragNodeExpandOverArea?: string;
   @ViewChild('emptyItem') emptyItem?: ElementRef;
 
-  constructor(private database: ChecklistDatabase) {
+  constructor(private database: ChecklistDatabase, private dialog: MatDialog) {
     this.treeFlattener = new MatTreeFlattener(
       this.transformer,
       this.getLevel,
@@ -238,7 +94,7 @@ export class PanelObjectsTreeComponent implements AfterViewInit {
 
   getChildren = (node: GameObject): GameObject[] => node.children;
 
-  hasChild = (_: number, _nodeData: GameObjectFlatNode) => _nodeData.expandable;
+  hasChild = (_nodeData: GameObjectFlatNode) => _nodeData.expandable;
 
   hasNoContent = (_: number, _nodeData: GameObjectFlatNode) =>
     _nodeData.item === '';
@@ -267,16 +123,17 @@ export class PanelObjectsTreeComponent implements AfterViewInit {
     return flatNode;
   };
 
-  /** Select the category so we can insert the new item. */
-  addNewItem(node: GameObjectFlatNode) {
-    const parentNode = this.flatNodeMap.get(node);
-    if (parentNode) {
-      this.database.insertItem(parentNode, {
-        name: '',
-        position: { x: 0, y: 0 },
-      });
-    }
-    this.treeControl.expand(node);
+  newObject(node?: GameObjectFlatNode) {
+    let parentNode: GameObject | undefined;
+    if (node) parentNode = this.flatNodeMap.get(node);
+
+    const dialogRef = this.dialog.open(DialogNewrectComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.database.insertItem(result, parentNode);
+        if (node) this.treeControl.expand(node);
+      }
+    });
   }
 
   /** Save the node to database */
@@ -341,7 +198,6 @@ export class PanelObjectsTreeComponent implements AfterViewInit {
         newItem = this.database.moveItem(dragNode, flatNodeMap);
       }
       if (newItem) {
-        // this.database.deleteItem(dragNode);
         const newnode = this.nestedNodeMap.get(newItem);
         if (!newnode) return;
         this.treeControl.expandDescendants(newnode);
---
+++ src/app/pages/edit/panels/panel-objects-tree/panel-objects-tree.database.ts
@@ -0,0 +1,123 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { GameObject, IGameObjectOptions } from 'star-gameengine';
+
+/**
+ * Checklist database, it can build a tree structured Json object.
+ * Each node in Json object represents a to-do item or a category.
+ * If a node is a category, it has children items and new items can be added under the category.
+ */
+@Injectable()
+export class ChecklistDatabase {
+  dataChange = new BehaviorSubject<GameObject[]>([]);
+
+  get data(): GameObject[] {
+    return this.dataChange.value;
+  }
+
+  constructor() {
+    const data: GameObject[] = [];
+    this.dataChange.next(data);
+  }
+
+  load(objects: GameObject[]) {
+    this.dataChange.next(objects);
+  }
+
+  /** Add an item to to-do list */
+  insertItem(newItem: GameObject, parent?: GameObject): GameObject {
+    if (parent) {
+      parent.addChild(newItem);
+    } else {
+      this.data.push(newItem);
+    }
+    this.dataChange.next(this.data);
+    return newItem;
+  }
+
+  getParentFromNodes(node: GameObject): GameObject | undefined {
+    for (const currentRoot of this.data) {
+      const parent = this.getParent(currentRoot, node);
+      if (parent != null) {
+        return parent;
+      }
+    }
+    return undefined;
+  }
+
+  getParent(currentRoot: GameObject, node: GameObject): GameObject | undefined {
+    if (currentRoot.children && currentRoot.children.length > 0) {
+      for (const child of currentRoot.children) {
+        if (child === node) {
+          return currentRoot;
+        } else if (child.children && child.children.length > 0) {
+          const parent = this.getParent(child, node);
+          if (parent != null) {
+            return parent;
+          }
+        }
+      }
+    }
+    return undefined;
+  }
+
+  updateItem(node: GameObject, name: string) {
+    node.name = name;
+    this.dataChange.next(this.data);
+  }
+
+  deleteItem(node: GameObject) {
+    this.deleteNode(this.data, node);
+    this.dataChange.next(this.data);
+  }
+
+  private removeOldParent(from: GameObject) {
+    const parent = this.getParentFromNodes(from);
+    const children = parent != null ? parent.children : this.data;
+    this.deleteNode(children, from);
+  }
+
+  moveItem(from: GameObject, to: GameObject): GameObject {
+    this.removeOldParent(from);
+
+    to.children.push(from);
+    from.setParent(to);
+    this.dataChange.next(this.data);
+    return from;
+  }
+
+  moveItemAtIndex(from: GameObject, to: GameObject, increment: number) {
+    this.removeOldParent(from);
+
+    const parentNode = this.getParentFromNodes(to);
+    const objs = parentNode != null ? parentNode.children : this.data;
+
+    const index = objs.indexOf(to);
+    objs.splice(index + increment, 0, from);
+
+    from.setParent(parentNode);
+    this.dataChange.next(this.data);
+    return from;
+  }
+
+  moveItemAbove(from: GameObject, to: GameObject): GameObject {
+    return this.moveItemAtIndex(from, to, 0);
+  }
+
+  moveItemBelow(from: GameObject, to: GameObject): GameObject {
+    return this.moveItemAtIndex(from, to, 1);
+  }
+
+  deleteNode(nodes: GameObject[], nodeToDelete: GameObject) {
+    const index = nodes.indexOf(nodeToDelete, 0);
+    if (index > -1) {
+      nodes.splice(index, 1);
+    } else {
+      nodes.forEach((node) => {
+        if (node.children && node.children.length > 0) {
+          this.deleteNode(node.children, nodeToDelete);
+        }
+      });
+    }
+  }
+}
